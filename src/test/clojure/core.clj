8d8962826df0ed5dad78b13e1d4a28b2a426fab3
Antti Harju <antti.harju@eficode.fi>
2011-04-24 01:21:46 +0300

    No need to explicitly run tests when working with IDE

--- src/test/clojure/core.clj
+++ src/test/clojure/core.clj
@@ -1,10 +1,8 @@
 (ns core
-  (:use [clojure.test :only [run-tests deftest is]])
+  (:use [clojure.test :only [deftest is]])
   (:use regexeval.core))
 
 (deftest dummy-test []
   (is (= (re-find #"Hello" dummy-text) "Hello")))
-
-(run-tests)
 
 
