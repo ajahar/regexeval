54ee5894602c31d8aad33e0a19a194d0f4f6dd76
Antti Harju <antti.harju@saunalahti.fi>
2011-09-14 19:11:32 +0300

    Migrate from Maven to Leiningen

--- src/test/clojure/core.clj
+++
@@ -1,8 +0,0 @@
-(ns core
-  (:use [clojure.test :only [deftest is]])
-  (:use regexeval.core))
-
-(deftest dummy-test []
-  (is (= (re-find #"Hello" (.getText label)) "Hello")))
-
-
--- src/test/clojure/run.clj
+++
@@ -1,5 +0,0 @@
-(ns run
-  "Start script for regexeval. Used from IDEs and with the Maven clojure:run task."
-  (:use regexeval.core))
-
-(-main)
