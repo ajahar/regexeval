db916545c869316f4306791bfe095be0e6bc21b9
Antti Harju <antti.harju@eficode.com>
2012-04-04 20:43:53 +0300

    Update seesaw

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject regexeval "1.0.0-SNAPSHOT"
   :description "A simple regular expression test application"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [seesaw "1.3.0"]]
+                 [seesaw "1.4.0"]]
   :main regexeval.core)
