247611ae67d469951afcb7dc45f3e90ea37b2081
Antti Harju <antti.harju@eficode.com>
2012-08-08 19:12:35 +0300

    Upgrade seesaw

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject regexeval "1.0.0-SNAPSHOT"
   :description "A simple regular expression test application"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.0"]]
+                 [seesaw "1.4.2"]]
   :main regexeval.core)
