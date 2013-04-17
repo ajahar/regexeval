298aff577b7536adb6f2bc89a3fa5e29e5627d71
Antti Harju <antti.harju@eficode.com>
2013-04-17 18:47:06 +0300

    Update to clojure 1.5.0 and seesaw 1.4.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject regexeval "1.0.0-SNAPSHOT"
   :description "A simple regular expression test application"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.2"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [seesaw "1.4.3"]]
   :main regexeval.core)
